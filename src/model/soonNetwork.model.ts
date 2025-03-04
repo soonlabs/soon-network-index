0345cd4cc83296540e477539bf86eec17ad00205
Senn <sennhanami@163.com>
2025-03-04 13:52:33 +0800

    add comment

--- src/model/soonNetwork.model.ts
+++ src/model/soonNetwork.model.ts
@@ -18,12 +18,15 @@ export class SoonNetworkStatus {
   @PrimaryColumn()
   id!: string;
 
+  // transaction count
   @BigIntColumn({ nullable: false })
   txCount!: bigint;
 
+  // address count
   @BigIntColumn({ nullable: false })
   addressCount!: bigint;
 
+  // program count
   @BigIntColumn({ nullable: false })
   programCount!: bigint;
 }
@@ -34,24 +37,31 @@ export class SoonNetworkTx {
     Object.assign(this, props);
   }
 
+  // tx hash
   @PrimaryColumn()
   id!: string;
 
+  // sender of the tx
   @StringColumn({ nullable: false })
   sender!: string;
 
+  // compute unit
   @BigIntColumn({ nullable: false })
   computeUnit!: bigint;
 
+  // prioritization gas price, unit is micro lamports
   @BigIntColumn({ nullable: false })
   prioritizationGasPrice!: bigint;
 
+  // tx fee
   @BigIntColumn({ nullable: false })
   fee!: bigint;
 
+  // token transfer in the tx
   @OneToMany(() => TokenTransfer, (ele) => ele.tx)
   tokenTransfer!: bigint;
 
+  // tx hash
   @StringColumn({ nullable: false })
   txHash!: string;
 
@@ -68,9 +78,11 @@ export class SoonNetworkUserAddress {
     Object.assign(this, props);
   }
 
+  // account address
   @PrimaryColumn()
   id!: string;
 
+  // last active timestamp of the address
   @BigIntColumn({ nullable: false })
   lastActiveTimestamp!: bigint;
 }
@@ -81,9 +93,11 @@ export class SoonNetworkProgram {
     Object.assign(this, props);
   }
 
+  // program address
   @PrimaryColumn()
   id!: string;
 
+  // last active timestamp of the program
   @BigIntColumn({ nullable: false })
   lastActiveTimestamp!: bigint;
 }
@@ -97,21 +111,27 @@ export class TokenTransfer {
   @PrimaryColumn()
   id!: string;
 
+  // tx
   @ManyToOne(() => SoonNetworkTx, (ele) => ele.txHash)
   tx!: SoonNetworkTx;
 
+  // previous owner of this token account
   @StringColumn({ nullable: true })
   preOwner!: string;
 
+  // post owner of this token account
   @StringColumn({ nullable: true })
   postOwner!: string;
 
+  // amount before the tx
   @BigIntColumn({ nullable: true })
   preAmount!: bigint;
 
+  // amount after the tx
   @BigIntColumn({ nullable: true })
   postAmount!: bigint;
 
+  // token address
   @StringColumn({ nullable: true })
   mint!: string;
 }
